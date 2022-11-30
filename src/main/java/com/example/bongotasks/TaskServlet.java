06191a9892a29f829e39d3720134e4184489954f
LaptopNat <semblancewarden@Gmail.com>
2022-11-30 15:43:20 +0800

    ✨ Modified TaskServlet
    - added Task class (taskName, taskDesc, status)

--- src/main/java/com/example/bongotasks/TaskServlet.java
+++ src/main/java/com/example/bongotasks/TaskServlet.java
@@ -7,6 +7,19 @@ import java.io.IOException;
 
 @WebServlet(name = "TaskServlet", value = "/TaskServlet")
 public class TaskServlet extends HttpServlet {
+
+    public class Task {
+        public String taskName;
+        public String taskDesc;
+        public boolean status;
+
+        public Task(String taskName, String taskDesc, boolean status) {
+            this.taskName = taskName;
+            this.taskDesc = taskDesc;
+            this.status = status;
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
