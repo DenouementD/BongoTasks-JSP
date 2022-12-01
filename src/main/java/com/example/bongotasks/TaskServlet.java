7baad251b034be782d3f674ba3f182db556fa319
Nitish <tnitish654@gmail.com>
2022-12-01 21:25:58 +0800

    ⚡Refactored sample tasks into a separate file

--- src/main/java/com/example/bongotasks/TaskServlet.java
+++ src/main/java/com/example/bongotasks/TaskServlet.java
@@ -8,30 +8,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet(name = "TaskServlet", value = "/TaskServlet")
 public class TaskServlet extends HttpServlet {
 
-    public List<Task> taskList = new ArrayList<Task>();
-
-    public void initTask() {
-        taskList.add(new Task("Buy Milk", "Buy some milk", false));
-        taskList.add(new Task("Buy Eggs", "Buy some eggs", true));
-        taskList.add(new Task("Cook breakfast", "cook some breakfast with milk and eggs", false));
-    }
+    SampleTasks sampleTasks = new SampleTasks();
 
     private void getTasks(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        session.setAttribute("taskList", taskList);
+        session.setAttribute("taskList", sampleTasks.taskList);
         request.getRequestDispatcher("/taskDashboard.jsp").forward(request, response);
 
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        initTask();
         getTasks(request, response);
     }
 
