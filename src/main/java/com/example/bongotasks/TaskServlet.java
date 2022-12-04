ad25f8f738d30907cdaca9f59f341549581fa032
Nitish <tnitish654@gmail.com>
2022-12-04 23:21:33 +0800

    ✨Tasks in dashboard are now fetched from mySQL
    ⚡Added id property to Task class

--- src/main/java/com/example/bongotasks/TaskServlet.java
+++ src/main/java/com/example/bongotasks/TaskServlet.java
@@ -6,28 +6,78 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "TaskServlet", value = "/TaskServlet")
 public class TaskServlet extends HttpServlet {
 
     SampleTasks sampleTasks = new SampleTasks();
 
-    private void getTasks(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        session.setAttribute("taskList", sampleTasks.taskList);
-        request.getRequestDispatcher("/taskDashboard.jsp").forward(request, response);
+    // connection to the database
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Variables that are used for database connections
+            String sqlURL = "jdbc:mysql://127.0.0.1:3306/?user=root";
+            String sqlUsername = "root";
+            String sqlPassword = "admin";
+            connection = DriverManager.getConnection(sqlURL, sqlUsername, sqlPassword);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
+        return connection;
+    }
+
+    private void getTasks(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        List<Task> taskList = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM bongotasks.tasks")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                boolean status = resultSet.getBoolean("status");
+                taskList.add(new Task(id, name, description, status));
+                System.out.println("Task: " + id + "|" + name + "|" + description + "|" + status);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        request.setAttribute("taskList", taskList);
+        request.getRequestDispatcher("/taskDashboard.jsp").forward(request, response);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getTasks(request, response);
+        try {
+            getTasks(request, response);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        System.out.println(request.getParameter("taskName"));
+//        System.out.println(request.getParameter("taskDesc"));
+//        sampleTasks.addTask(new Task(, request.getParameter("taskName"), request.getParameter("taskDesc"), false));
+//        request.getRequestDispatcher("/taskDashboard.jsp").forward(request, response);
+    }
 
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
     }
 }
