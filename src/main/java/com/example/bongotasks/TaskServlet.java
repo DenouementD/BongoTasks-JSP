27c2328a57ea8d0b44a5f204800ab39aaf5926cb
Nitish <79391172+TechSupportz@users.noreply.github.com>
2022-12-05 14:45:08 +0800

    ✨ Migrated TaskServlet from sample data to MySQL (#16)
    * ✨Tasks in dashboard are now fetched from mySQL
    
    ⚡Added id property to Task class
    
    * ⚡Added switch case to call CRUD functions based on servlet route
    
    ⚡Implemented empty methods for add, update and delete tasks

--- src/main/java/com/example/bongotasks/TaskServlet.java
+++ src/main/java/com/example/bongotasks/TaskServlet.java
@@ -9,34 +9,91 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.Console;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "TaskServlet", value = "/TaskServlet")
 public class TaskServlet extends HttpServlet {
 
     SampleTasks sampleTasks = new SampleTasks();
 
-    private void getTasks(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        session.setAttribute("taskList", sampleTasks.taskList);
-        request.getRequestDispatcher("/taskDashboard.jsp").forward(request, response);
+    // connection to the database
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Variables that are used for database connections
+            String sqlURL = "jdbc:mysql://127.0.0.1:3306/?user=root";
+            String sqlUsername = "root";
+            String sqlPassword = "admin";
+            connection = DriverManager.getConnection(sqlURL, sqlUsername, sqlPassword);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
+        return connection;
     }
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+    private void getTasks(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        List<Task> taskList = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM bongotasks.tasks")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                boolean status = resultSet.getBoolean("status");
+                taskList.add(new Task(id, name, description, status));
+                System.out.println("Task: " + id + "|" + name + "|" + description + "|" + status);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        request.setAttribute("taskList", taskList);
+        request.getRequestDispatcher("/taskDashboard.jsp").forward(request, response);
+    }
+
+    private void addTask(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        // @PuttTim - Please add the code to add a task to the database
+    }
+
+    private void updateTask(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        // @UnscriptedLogic - Please add the code to update a task in the database
+    }
+
+    private void deleteTask(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        // @DenouementD - Please add the code to delete a task in the database
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getTasks(request, response);
+        String servletPath = request.getServletPath();
+        try {
+            switch (servletPath) {
+                case "/TaskServlet/addTask":
+                    addTask(request, response);
+                    break;
+                case "/TaskServlet/updateTask":
+                    updateTask(request, response);
+                    break;
+                case "/TaskServlet/deleteTask":
+                    deleteTask(request, response);
+                    break;
+                default:
+                    getTasks(request, response);
+                    break;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println(request.getParameter("taskName"));
-        System.out.println(request.getParameter("taskDesc"));
-        sampleTasks.addTask(new Task(request.getParameter("taskName"), request.getParameter("taskDesc"), false));
-        request.getRequestDispatcher("/taskDashboard.jsp").forward(request, response);
+        doGet(request, response);
+
     }
 }
